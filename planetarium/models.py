dcb93838ec716aa558d30ab501ff96301056bc50
Anton Komarov <anton_komarov_qa@ukr.net>
2023-08-09 14:31:13 +0300

    'Modify validation for Tickets'

--- planetarium/models.py
+++ planetarium/models.py
@@ -82,33 +82,40 @@ class Ticket(models.Model):
         Reservation, on_delete=models.CASCADE, related_name="tickets"
     )
 
-    def clean(self):
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
         for ticket_attr_value, ticket_attr_name, planetarium_dome_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
         ]:
-            count_attrs = getattr(
-                self.show_session.planetarium_dome, planetarium_dome_attr_name
-            )
+            count_attrs = getattr(planetarium_dome, planetarium_dome_attr_name)
             if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
+                raise error_to_raise(
                     {
                         ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {planetarium_dome_attr_name}): "
-                        f"(1, {count_attrs})"
+                                          f"number must be in available range: "
+                                          f"(1, {planetarium_dome_attr_name}): "
+                                          f"(1, {count_attrs})"
                     }
                 )
 
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
     def save(
-        self,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
     ):
         self.full_clean()
-        super(Ticket, self).save(
+        return super(Ticket, self).save(
             force_insert, force_update, using, update_fields
         )
 
@@ -119,3 +126,4 @@ class Ticket(models.Model):
 
     class Meta:
         unique_together = ("show_session", "row", "seat")
+        ordering = ["row", "seat"]
