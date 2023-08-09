34420e2494ef169b1bb2fcf25ec0cb73691e1d07
Anton Komarov <anton_komarov_qa@ukr.net>
2023-08-09 17:51:54 +0300

    'Include planetarium.urls in config.urls'

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
     path("api/user/", include("user.urls", namespace="user")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
