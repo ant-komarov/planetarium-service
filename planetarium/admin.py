905a697508d5851a9f7068527754563a5f40dc83
Anton Komarov <134077824+ant-komarov@users.noreply.github.com>
2023-08-10 19:35:32 +0300

    Revert "Revert "Develop""

---
+++ planetarium/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+
+
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
